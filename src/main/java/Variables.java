4bf1a1dac64b4fde72616e683b6c4da84bc2fb1f
Yuliya Bezlatneva <bezlatneva@yandex.com>
2023-10-14 18:20:21 +0700

    Adding variables

---
+++ src/main/java/Variables.java
@@ -0,0 +1,9 @@
+public class Variables {
+    public static void main(String[] args) {
+        double doubleVar = 11.99d;
+        byte byteMax = 127;
+
+        System.out.println("Произвольное дробное значение переменной типа с плавающей точкой с наибольшим диапазоном чисел: " + doubleVar);
+        System.out.println("Hаибольшее возможное значение переменной целочисленного типа с наименьшим диапазоном чисел: " + byteMax);
+    }
+}
